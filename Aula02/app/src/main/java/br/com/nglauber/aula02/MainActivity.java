ed95a17e0e8fbe9a391f26ae6753c9d7ca145508
Nelson Glauber <nglaubervasc@gmail.com>
2017-04-08 14:50:53 -0300

    Exemplo da aula 2 com ButterKnife

--- Aula02/app/src/main/java/br/com/nglauber/aula02/MainActivity.java
+++ Aula02/app/src/main/java/br/com/nglauber/aula02/MainActivity.java
@@ -2,15 +2,18 @@ package br.com.nglauber.aula02;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtGasolina;
-    EditText edtEtanol;
-    TextView txtResultado;
+    @BindView(R.id.edit_gasolina) EditText edtGasolina;
+    @BindView(R.id.edit_etanol) EditText edtEtanol;
+    @BindView(R.id.text_resultado) TextView txtResultado;
 
     int resultado;
 
@@ -18,11 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        edtGasolina = (EditText) findViewById(R.id.edit_gasolina);
-        edtEtanol = (EditText) findViewById(R.id.edit_etanol);
-        txtResultado = (TextView) findViewById(R.id.text_resultado);
-        findViewById(R.id.button_calcular).setOnClickListener(onClick);
         if (savedInstanceState != null){
             resultado = savedInstanceState.getInt("calculo");
             if (resultado != 0) {
@@ -37,19 +37,17 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt("calculo", resultado);
     }
 
-    private View.OnClickListener onClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            float gas = Float.parseFloat(edtGasolina.getText().toString());
-            float etanol = Float.parseFloat(edtEtanol.getText().toString());
-            if (etanol < gas * .7f){
-                resultado = R.string.texto_etanol;
-            } else {
-                resultado = R.string.texto_gasolina;
-            }
-            exibirResultado();
+    @OnClick(R.id.button_calcular)
+    public void onCalcularClick() {
+        float gas = Float.parseFloat(edtGasolina.getText().toString());
+        float etanol = Float.parseFloat(edtEtanol.getText().toString());
+        if (etanol < gas * .7f){
+            resultado = R.string.texto_etanol;
+        } else {
+            resultado = R.string.texto_gasolina;
         }
-    };
+        exibirResultado();
+    }
 
     private void exibirResultado(){
         String opcao = getString(resultado);
